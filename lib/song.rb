0016136591ef610a1f9023657212c304ec7bbdb2
Tyler Hunt <tyhunt04@gmail.com>
2019-02-21 19:32:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,14 +29,26 @@ class Song
 
   def self.genre_count
     genre_count = {}
-    @@genres.each do |list|
-      if genre_count[list]
-        genre_count[list] += 1
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
     else
-      genre_count[list] = 1
+      genre_count[genre] = 1
     end
   end
     genre_count
+  end
+
+  def self.artist_count
+  artist_count = {}
+    @@artists.each do |singer|
+      if artist_count[singer]
+        artist_count[singer] += 1
+    else
+      artist_count[singer] = 1
+    end
+  end
+    artist_count
   end
 
 
