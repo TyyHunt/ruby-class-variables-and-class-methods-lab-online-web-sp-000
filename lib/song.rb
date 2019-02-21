085d00fd9ee92cb1b666e3f9ac1cc1de9997d1ce
Tyler Hunt <tyhunt04@gmail.com>
2019-02-21 19:27:41 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,37 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(song_title, artist, genre)
+    @name = song_title
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq!
+  end
+
+  def self.genres
+    @@genres.uniq!
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each {|list| genre_count[list] += 1}
+    genre_count
+  end
+
+
+end
